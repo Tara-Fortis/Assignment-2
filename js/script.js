af0e8fb898c28ec2c9de0ce6d5589a28960090b4
Tineil_A <tine6573@gmail.com>
2025-11-14 12:35:01 -0500

    debug navbar collapse component

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
 // current date
 document.getElementById("year").innerHTML = new Date().getFullYear();
 // from https://stackoverflow.com/questions/6002254/get-the-current-year-in-javascript
-
-const bootstrap = require('bootstrap');
