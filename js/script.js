11fdc86d578bf56a0919b2a5499fb0c1ae2ee00b
Tineil_A <tine6573@gmail.com>
2025-11-14 12:32:03 -0500

    Update code structure

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 // current date
 document.getElementById("year").innerHTML = new Date().getFullYear();
 // from https://stackoverflow.com/questions/6002254/get-the-current-year-in-javascript
+
+const bootstrap = require('bootstrap');
