b99eb45461a03bd697b3f81fe894e5709318081c
Tineil_A <tine6573@gmail.com>
2025-11-14 13:20:24 -0500

    Started carousel for book covers

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 // current date
 document.getElementById("year").innerHTML = new Date().getFullYear();
 // from https://stackoverflow.com/questions/6002254/get-the-current-year-in-javascript
+
+// Bootstrap Carousel
+const myCarousel = new bootstrap.Carousel(myCarouselElement, {
+    cycle: true,
+    interval: 2000,
+    pause: hover,
+    touch: false
+})
